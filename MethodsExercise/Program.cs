b475a9af6d90d33bf35c8771fac0e10e1cdf739b
eopong87 <eopong87@gmail.com>
2023-06-19 14:43:25 -0400

    done

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace MethodsExercise
+﻿
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //-------Exercise 1--------
+            // Name: Eddie
+            // Favorite Color: Red
+            // Favorite Animal: Tiger
+            // Favorite Band: Wu-Tang Clan
+
+            Console.WriteLine("Hello- What is your name?");
+            var username = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {username}.  what is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an awesome color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine(" Thats cool! Who is your favorite band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"Name:{username}");
+            Console.WriteLine($"We saw a {color} {animal} walking down the street");
+            Console.WriteLine($"The big {animal} roared at us as we ran across the street.");
+            Console.WriteLine($"we noticed he was listening to {band}.");
+            Console.ReadLine();
         }
     }
 }
+
+
+
