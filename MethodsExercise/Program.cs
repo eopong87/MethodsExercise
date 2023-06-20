820a36b0cc540ed970694961399734cdc2a754f0
eopong87 <eopong87@gmail.com>
2023-06-20 09:30:47 -0400

    update

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,8 +4,52 @@ namespace MethodsExercise
 {
     public class Program
     {
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer; }
+
+        
+       
+        
+        
+
+        public static int Subtract(int num1, int num2)
+        {
+            var answer = num1 - num2;
+            return answer;
+        }
+
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+
+
+        }
+        public static int Divide(int num1, int num2)
+            {
+                return num1 / num2 ;
+            }
+
+        public static int modulus(int num1, int num2)
+        { return num1 % num2 ; }
+
         static void Main(string[] args)
         {
+            var amountOfBalls = Sum(2, 6);
+            var toys = Multiply(60, 2, 4);
+                var shoes = Subtract(50, 25);
+                var clothes = Divide(50, 25);
+            var cups = modulus(10, 3);
+            
+
+            Console.WriteLine(amountOfBalls);
+            Console.WriteLine(toys);
+                Console.WriteLine(shoes);
+                Console.WriteLine(clothes);
+            Console.WriteLine(cups);
+               
+
             //-------Exercise 1--------
             // Name: Eddie
             // Favorite Color: Red
@@ -29,9 +73,12 @@ namespace MethodsExercise
             Console.WriteLine($"The big {animal} roared at us as we ran across the street.");
             Console.WriteLine($"we noticed he was listening to {band}.");
             Console.ReadLine();
+            //------------------exercise 2------------------
+          
+
         }
     }
-}
 
+}
 
 
